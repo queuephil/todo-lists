cc30b115742c4ec4e60d1d5da5ce567ab9f7a570
pquauke <philipp.quauke@gmail.com>
2024-11-23 20:10:48 +0100

    On my way again

--- src/modules.js
+++ src/modules.js
@@ -11,13 +11,15 @@ const DOM = (function() {
         parentSelector, 
         htmlTag, 
         innerHTML,
-        attributes = {}
+        attributes = {},
+        // childs = {},
     ) {
         return { 
             parentSelector, 
             htmlTag,
             innerHTML,
-            attributes
+            attributes, 
+            // childs,
         };
     };
     
@@ -25,7 +27,7 @@ const DOM = (function() {
     const addElement = function addElement({ 
         parentSelector, 
         htmlTag, 
-        innerHTML ="",
+        innerHTML = " ", // can't be empty, otherwise it would delete the item itself
         attributes = {}
     }) {
         const newChild = document.createElement(htmlTag);
@@ -41,4 +43,79 @@ const DOM = (function() {
     return { defineElement, addElement };
 })();
 
-export {DOM}
+const OBJ = (function() {
+
+    const changeParent = function(object, oldParent, newParent) {
+        newParent[object] = oldParent[object];
+        delete oldParent[object];
+    }
+    
+    // use an object with key "prop" to define the values----------------------
+    const flattenForDOMaddElement = function(object, parentSelector = "container") {
+        const flattenedObject = [];
+        for (const [key, value] of Object.entries(object)) {
+            if (value.prop?.innerHTML) {
+                flattenedObject.push({
+                    // define key of parentObject as parentSelector
+                    parentSelector: `.${parentSelector}`, 
+                    // get the other properties
+                    htmlTag: value.prop.htmlTag, 
+                    innerHTML: value.prop.innerHTML || "",
+                    attributes: value.prop.attributes,
+                });
+            }
+            if (typeof value === "object" && !Array.isArray(value)) {
+                flattenedObject.push(...flattenForDOMaddElement(value, key));
+            }
+        }
+        return flattenedObject;
+    }
+
+    // order an object by its values-----------------------------------------------
+    // https://stackoverflow.com/questions/1069666/sorting-object-property-by-values
+    // const list1 = {
+    //     item1: 3,
+    //     item2: 2,
+    //     item3: 1,
+    // }
+    // const sortedObject = Object.fromEntries(
+    //     Object.entries(list1).sort(([,a],[,b]) => a - b)
+    // )
+    // console.log(list1)
+    // console.log(sortedObject)
+
+    // const list1 = {
+    //     prop: {
+    //         htmlTag: 'div',
+    //         innerHTML: 'list1',
+    //     },
+    //     item1: {
+    //         prop: {
+    //             htmlTag: 'div',
+    //             innerHTML: 'item1',
+    //         },
+    //         subitem1: {
+    //             prop: {
+    //                 htmlTag: 'div',
+    //                 innerHTML: 'item1.1',
+    //             },
+    //         }
+    //     },
+    //     item2: {
+    //         prop: {
+    //             htmlTag: 'div',
+    //             innerHTML: 'item2',
+    //         },
+    //         subitem2: {
+    //             prop: {
+    //                 htmlTag: 'div',
+    //                 innerHTML: 'item2.1',
+    //             },
+    //         }
+    //     }
+    // }
+    
+    return { changeParent, flattenForDOMaddElement }
+})();
+
+export { DOM, OBJ }
--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // console.log("test");
 import "./style.css";
-import {DOM} from "./modules.js"
+import {DOM, OBJ} from "./modules.js"
 
 // add "list"
     // functions                    (N°3)
@@ -13,79 +13,151 @@ import {DOM} from "./modules.js"
     // functions
         // add new                  done
         // add text                 done
-        // add date                 (N°7)
-        // add important            
         // remove                   
         // move up/down             
         // make child/sibling       
         // collapse / expand        
         // check                    
+        // add date                 (N°7)
+        // add important            
         
-// main________________________________________________________________________
+// here happens all the magic__________________________________________________
+let desk = {};
+addUserInterface();
 
-let allItems = [];
-loadUserInterface();
-addNewItem();
-getLocalStorage();
-addItemsToDOM();
-saveEditedText(".itemText");
-preventDefaultEnter(".itemText");
+// if something is assigned to an object the key and class have to be the same!!
 
-// load UI---------------------------------------------------------------------
-function loadUserInterface(){
-    const elements = [
-        DOM.defineElement("main", "div", "", {class: "list"}),
-        DOM.defineElement("main", "div", "add", 
-            {class: "material-symbols-outlined addIcon"}),
-    ];
-    elements.forEach((element) => DOM.addElement(element));
+function addUserInterface(){
+    const userInterface = {
+        noteBook1: {
+            prop: {
+                htmlTag: "div",
+                innerHTML: "noteBook1",
+                attributes: {class: "noteBook1 noteBook"},
+            },
+        },
+        addIcon: {
+            prop: {
+                htmlTag: "div",
+                innerHTML: "add",
+                attributes: {class: "material-symbols-outlined addIcon"},
+            },
+        },
+    }
+    Object.assign(desk, userInterface)
+    addItemsToDOM(desk)
 }
 
-// add new item----------------------------------------------------------------
-function addNewItem() {
+function addNewNote() {
     document.querySelector(".addIcon").addEventListener("click", () => {
         const timestamp = Date.now();
-        const elements = [
-            DOM.defineElement(".list", "div", "", {class: "item", "data-id": timestamp,}),
-            DOM.defineElement(`div[data-id='${timestamp}']`, "div", "created item",
-                {class: "itemText", "data-id": timestamp + 1, contenteditable: "true",}
-            ),];
-        allItems = [...allItems, ...elements]; 
-        setLocalStorage();
-        addItemsToDOM();
-    });
+        const newNote = {
+            [`note${timestamp+1}`]: {
+                prop: {
+                    htmlTag: "div",
+                    innerHTML: " ",
+                    dataId: timestamp + 1,
+                    attributes: {
+                        "data-id": timestamp + 1,
+                        class: `note${timestamp+1} note`},
+                },
+                text: {
+                    prop: {
+                        htmlTag: "div",
+                        innerHTML: "...",
+                        dataId: timestamp + 2,
+                        attributes: {
+                            "data-id": timestamp + 2,
+                            class: "text", 
+                            contenteditable: "true"},
+                    },
+                },
+                deleteIcon: {
+                    prop: {
+                        htmlTag: "div",
+                        innerHTML: "delete",
+                        dataId: timestamp + 3,
+                        attributes: {
+                            "data-id": timestamp + 3,
+                            class: "material-symbols-outlined deleteIcon"},
+                    },
+                }
+            },
+        }
+        Object.assign(desk.noteBook1, newNote)
+        addItemsToDOM(desk)
+        removeElement(".deleteIcon", desk)
+    })
 }
 
-// add items to the DOM
-function addItemsToDOM() {
-    document.querySelector(".list").innerHTML ="";
-    allItems.forEach((element) => DOM.addElement(element));
+function addItemsToDOM(object) {
+    const flattenedArray = OBJ.flattenForDOMaddElement(object);
+    document.querySelector(".container").innerHTML ="";
+    flattenedArray.forEach((element) => {
+        DOM.addElement(element)
+    })
+    addNewNote();
+    saveEditedText(".text", desk);
 } 
 
-// add possiblity to edit the text---------------------------------------------
-function allignIndices(index) {return (index + 1) * 2 - 1}
-
-function saveEditedText(editableElement) {
-    const items = document.querySelectorAll(editableElement);
-    items.forEach((item, index) => {
-        item.addEventListener("blur", () => {          
-            const allItemsIndex = allignIndices(index);
-            allItems[allItemsIndex].innerHTML = item.innerHTML;
-            setLocalStorage();
+function saveEditedText(textElement, dataObject) {
+    document.querySelectorAll(textElement).forEach((element) => {
+        element.addEventListener("blur", () => {
+            const newText = element.innerText || element.textContent; 
+            const dataId = element.dataset.id; 
+            changeObjectsText(dataObject, dataId, newText);
         });
     });
 }
 
-// prevent default enter ------------------------------------------------------
-function preventDefaultEnter(element) {
-    document.querySelectorAll(element).forEach(itemText => {
-        itemText.addEventListener('keydown', (e) => {
-            if (e.key === 'Enter' && !e.shiftKey) {
+function changeObjectsText(object, id, newValue) {
+    for (const key in object) {
+        if (object[key].dataId == id) {
+            object[key].innerHTML = newValue; // this changes the object
+            return; 
+        }
+        if (typeof object[key] == "object") {
+            changeObjectsText(object[key], id, newValue); 
+        }
+    }
+}
+
+// prevent default enter-------------------------------------------------------
+function preventDefaultEnter(textElement) {
+    document.querySelectorAll(textElement).forEach(element => {
+        element.addEventListener("keydown", (e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
                 e.preventDefault();
-                itemText.blur();
+                element.blur();
             }
         });
     });
+}
+
+// remove item function--------------------------------------------------------
+function removeElement(buttonElement, dataObject) {
+    document.querySelectorAll(buttonElement).forEach(element => {
+        element.addEventListener("click", () => {
+            const dataIdObject = element.dataset.id;
+            const parentElement = document.querySelector(`[data-id="${dataIdObject}"]`).parentElement
+            const dataId = parentElement.dataset.id
+            // console.log(dataIdObject)
+            // console.log(dataId)
+            deleteObject(dataObject, dataId);
+        })
+    })
+}
+
+function deleteObject(object, id) {
+    for (const key in object) {
+        if (object[key].dataId == id) {
+            delete object[key];
+            return; 
+        }
+        if (typeof object[key] == "object") {
+            deleteObject(object[key], id); 
+        }
+    }
 }
 
 // local storage_______________________________________________________________
